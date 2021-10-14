6f97891a6d0ca8063b9098225a3e6e77d7e9b757
Maicon Alegre <maicon.rocha@dnx.solutions>
2021-10-14 22:30:52 +0000

    update outputs.tf

--- _outputs.tf
+++ _outputs.tf
@@ -1,7 +1,16 @@
-# output "mount_target_dns_name" {
-#   value = aws_efs_mount_target.mount_target.dns_name
-# }
+output "mount_target_id" {
+  value = { for id in keys(aws_efs_mount_target.mount_target) : id => aws_efs_mount_target.mount_target[id].id }
+}
 
-# output "mount_target_file_system_arn" {
-#   value = aws_efs_mount_target.mount_target.*.file_system_arn
-# }
+output "file_system_arn" {
+  value = aws_efs_file_system.efs.arn
+}
+
+output "file_system_id" {
+  value = aws_efs_file_system.efs.id
+}
+
+output "access_point_ids" {
+  value       = { for id in keys(aws_efs_access_point.efs_access_point) : id => aws_efs_access_point.efs_access_point[id].id }
+  description = "EFS AP ids"
+}
